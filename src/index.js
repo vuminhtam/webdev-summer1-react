92b1fd583b3c4ee8e8b6c42c3002d248213c4ad0
Tam Vu <vu.t@husky.neu.edu>
2018-05-23 21:04:55 -0400

    add userId to courseManager

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import Route, Router from 'react-router'
 import ReactDOM from 'react-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 
 import CourseManager from "./containers/CourseManager";
 import NavBar from "./component/Navbar";
+import CourseList from "./containers/CourseList";
 
 ReactDOM.render(
+    <Router>
     <div>
         <NavBar/>
+        <Route path="/uid/:uid"
+               component={CourseList}>
+        </Route>
         <CourseManager/>
-    </div>,
+    </div>
+    </Router>,
     document.getElementById('root')
 );
